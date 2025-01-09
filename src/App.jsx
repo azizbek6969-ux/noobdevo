5fa275912e7c8c68bc04fd4ae06d795343d87306
azizbek6969-ux <azizbekhaydarov333@gmail.com>
2025-01-09 12:38:36 +0500

    Fayllarni qo'shish

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import { Button } from './components/ui/button'
+function App() {
+  return (
+    <>
+      <div className='bg-gray-600  h-40 w-40  '>
+        <Button variant='destructive' >app</Button>
+      </div>
+    </>
+  )
+}
+export default App
