17ebb210664dd24a22d791f3f84437c51a07b29a
azizbek6969-ux <azizbekhaydarov333@gmail.com>
2025-01-09 13:04:40 +0500

    Faylni qo'shish yoki o'zgartirish

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import { Button } from './components/ui/button'
 function App() {
   return (
     <>
-      <div className='bg-gray-600  h-40 w-40  '>
-        <Button variant='destructive' >app</Button>
+      <div className=' '>
+
       </div>
     </>
   )
